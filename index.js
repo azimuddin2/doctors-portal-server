2ea87a6e51770768cf33073c964560f96fc7f806
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-01-29 10:33:13 +0600

    doctor portal app vercel deploy

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
@@ -32,8 +31,6 @@ function verifyJWT(req, res, next) {
         next();
     });
 };
-
-
 
 
 async function run() {
